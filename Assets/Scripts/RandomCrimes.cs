f0a75ff2c3e76c132c51b4f1636f3b75c5444339
Samuel <samuelessemail@gmail.com>
2026-02-09 21:40:31 +0000

    Worked on maze level

--- Assets/Scripts/RandomCrimes.cs
+++ Assets/Scripts/RandomCrimes.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RandomCrimes : MonoBehaviour
 {
+    public Material targetMaterial;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -37,17 +39,39 @@ public class RandomCrimes : MonoBehaviour
             "Entering a building"
         };
         
+        string[] seasons = {
+            "Christmas",
+            "Summer",
+            "Spring",
+            "Fall"
+        };
+        
         int crimeIndex = Random.Range(0, crimes.Length);
         int shirtIndex = Random.Range(0, shirtColors.Length);
         int actionIndex = Random.Range(0, actions.Length);
+        int seasonIndex = Random.Range(0, seasons.Length);
         
         string chosenCrime = crimes[crimeIndex];
         string chosenShirtColor = shirtColors[shirtIndex];
         string chosenAction = actions[actionIndex];
+        string chosenSeason = seasons[seasonIndex];
+        
+        if (targetMaterial != null)
+        {
+            if (chosenSeason == "Christmas")
+            {
+                targetMaterial.color = Color.white;
+            }
+            else if (chosenSeason == "Summer")
+            {
+                targetMaterial.color = Color.green;
+            }
+        }
         
         Debug.Log("Crime: " + chosenCrime);
         Debug.Log("Shirt Color: " + chosenShirtColor);
         Debug.Log("Action: " + chosenAction);
+        Debug.Log("Season: " + chosenSeason);
     }
 
     // Update is called once per frame
